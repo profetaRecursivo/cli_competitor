34c0cffb86637ef847736e8c204733760ffbc554
Jaime Sebastian Chavarria Fuertes <202301300@est.umss.edu>
2025-03-02 16:24:15 -0400

    files

--- src/gencomp.cpp
+++ src/gencomp.cpp
@@ -5,28 +5,33 @@
 
 using namespace std;
 
-string sanitize_name(const string &name) {
-    string sanitized = name;
-    for (char &c : sanitized) {
+string limpiarnombre(string &name) {
+    string ans = name;
+    for (char &c : ans) {
         if (c == ' ') c = '_';
     }
-    return sanitized;
+    return ans;
 }
 
-void create_directory(const string &path) {
+void crearDirectorio( string &path) {
     if (mkdir(path.c_str(), 0777) == -1 && errno != EEXIST) {
         cerr << "Error al crear el directorio: " << path << endl;
         exit(1);
     }
 }
 
-void create_files(const string &path, int n, const string &snippet) {
-    vector<string> names = {"a", "b", "c", "d", "e", "f", "g", "h"};
-    for (int i = 0; i < n && i < (int)names.size(); i++) {
-        string file_path = path + "/" + names[i] + ".cpp";
-        ofstream file(file_path);
+void crearArchivos(string &path, int n, string &snippet) {
+    for (int i = 0; i < n; i++) {
+        string nombreArchivo;
+        int num = i;
+        while (num >= 0) {
+            nombreArchivo = char('a' + (num % 26)) + nombreArchivo;
+            num = num / 26 - 1;
+        }
+        string ruta = path + "/" + nombreArchivo + ".cpp";
+        ofstream file(ruta);
         if (!file) {
-            cerr << "Error al crear el archivo: " << file_path << endl;
+            cerr << "Error al crear el archivo: " << ruta << endl;
             exit(1);
         }
         file << snippet;
@@ -34,7 +39,8 @@ void create_files(const string &path, int n, const string &snippet) {
     }
 }
 
-string read_snippet(const string &snippet_path) {
+
+string leerssnipet(const string &snippet_path) {
     ifstream file(snippet_path);
     if (!file) {
         cerr << "Error al abrir el snippet: " << snippet_path << endl;
@@ -51,26 +57,26 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     
-    string competition_name = argv[1];
-    string competition_path = getenv("HOME") + string("/competencias/") + sanitize_name(competition_name);
-    int num_problems = 0;
+    string nombreCompetencia = argv[1];
+    string rutaBase = getenv("HOME") + string("/competencias");
+    string rutaCompetencia = rutaBase + "/" + limpiarnombre(nombreCompetencia);
+    int numProblemas = 0;
     string snippet;
 
     for (int i = 2; i < argc; i++) {
         string arg = argv[i];
         if (arg == "-n" && i + 1 < argc) {
-            num_problems = stoi(argv[++i]);
+            numProblemas = stoi(argv[++i]);
         } else if (arg == "-snp" && i + 1 < argc) {
-            snippet = read_snippet(argv[++i]);
+            snippet = leerssnipet(argv[++i]);
         }
     }
     
-    create_directory(competition_path);
-    create_files(competition_path, num_problems, snippet);
-    
-    // Abrir VS Code en la carpeta de la competencia y cerrar la terminal
-    string command = "code " + competition_path + " && exit";
+    crearDirectorio(rutaBase);
+    crearDirectorio(rutaCompetencia);
+
+    crearArchivos(rutaCompetencia, numProblemas, snippet);
+    string command = "code " + rutaCompetencia + " && exit";
     system(command.c_str());
-    
     return 0;
 }
